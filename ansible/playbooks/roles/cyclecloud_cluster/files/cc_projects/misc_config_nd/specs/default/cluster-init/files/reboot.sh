a7e39a30fd564ab0bebab04d28c29548423c840f
Davide Vanzo <davide.vanzo@microsoft.com>
2023-12-21 10:56:00 -0600

    Added RAID array creation

---
+++ ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/misc_config_nd/specs/default/cluster-init/files/reboot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPTS_DIR=/root
+
+if [ -b /dev/md127 ]; then
+   DEV=/dev/md127
+elif [ -b /dev/md128 ]; then
+   DEV=/dev/md128
+else
+   ${SCRIPTS_DIR}/setup_nvme_heal.sh
+fi
+
+if [ -n "$DEV" ]; then
+   mount $DEV /mnt/resource_nvme
+fi
