cd45772561ec6357961798c94f64f20af64ec4f3
Davide Vanzo <davide.vanzo@microsoft.com>
2024-01-02 02:26:29 -0600

    Reverted back to PMIx v3

--- ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/install_pmix/specs/default/cluster-init/scripts/00_install_pmix.sh
+++ ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/install_pmix/specs/default/cluster-init/scripts/00_install_pmix.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+PMIX_VERSION='3.2.5'
+
 function is_slurm_controller() {
     ls /lib/systemd/system/ | grep -q slurmctld
 }
 
 if is_slurm_controller; then
-    mkdir -p /sched/pmix/v4
+    mkdir -p /sched/pmix
     apt install -y libevent-dev libhwloc-dev
-    tar xzf $CYCLECLOUD_SPEC_PATH/files/pmix-4.2.8.tar.gz
-    cd pmix-4.2.8
-    ./configure --prefix=/sched/pmix/v4
+    tar xzf $CYCLECLOUD_SPEC_PATH/files/pmix-${PMIX_VERSION}.tar.gz
+    cd pmix-${PMIX_VERSION}
+    ./configure --prefix=/sched/pmix
     make
 	make install
 	cd ..
-	rm -rf pmix-4.2.8
+	rm -rf pmix-${PMIX_VERSION}
 fi
 
 # This Slurm build is not configured to find PMIx libraries at /opt/pmix/v4
-ln -s /sched/pmix/v4/lib/libpmix.so /usr/lib/libpmix.so
+ln -s /sched/pmix/lib/libpmix.so /usr/lib/libpmix.so
 
 if is_slurm_controller; then
 	systemctl restart slurmctld
