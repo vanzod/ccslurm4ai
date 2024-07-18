bfeeabeb5ff24c42786381bb048db3720d59bded
Davide Vanzo <davide.vanzo@microsoft.com>
2024-07-18 16:10:37 -0500

    Removed unnecessary projects

--- ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/fix_accounting_storage_host/specs/default/cluster-init/scripts/00_set_scheduler_acct_storage_host.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "AccountingStorageHost=$(hostname)" >> /etc/slurm/accounting.conf
--- ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/fix_slurm_memory_dampen/specs/default/cluster-init/scripts/00_update_azslurm_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script is used to update the azslurm library to fix the memory dampening issue.
-# This workaround will should no longer be required for azslurm versions >= 3.0.7
-set -e
-
-for p in util.py cli.py partition.py ; do
-    wget -O /opt/azurehpc/slurm/venv/lib/python3.*/site-packages/slurmcc/${p} https://raw.githubusercontent.com/Azure/cyclecloud-slurm/d926bb53ed4dd37df5e3f3729919b3fb2475aa50/slurm/src/slurmcc/${p}
-done
-/root/bin/azslurm scale
