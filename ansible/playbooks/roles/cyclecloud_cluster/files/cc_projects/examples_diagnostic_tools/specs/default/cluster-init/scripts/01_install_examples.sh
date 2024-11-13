8eff43e74b5e6038428e612d1ef0f9f60c1d522a
Davide Vanzo <davide.vanzo@microsoft.com>
2024-11-13 14:57:47 -0600

    Fix files ownership

--- ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/examples_diagnostic_tools/specs/default/cluster-init/scripts/01_install_examples.sh
+++ ansible/playbooks/roles/cyclecloud_cluster/files/cc_projects/examples_diagnostic_tools/specs/default/cluster-init/scripts/01_install_examples.sh
@@ -5,4 +5,5 @@ ROOT_DIR=/shared/home/cycleadmin
 
 if [ ! -d $ROOT_DIR/examples ]; then
     cp -r $CYCLECLOUD_SPEC_PATH/files/examples $ROOT_DIR/examples
+    chown -R cycleadmin:cycleadmin $ROOT_DIR/examples
 fi
